758623b1aa61c1c8fd62a0d4c8a572d7efd2ff16
jbmi2000 <jeff.marbury@gmail.com>
2020-02-15 02:19:32 -0500

    Working with scraper class

--- lib/hairsalon/scraper.rb
+++ lib/hairsalon/scraper.rb
@@ -1,9 +1,9 @@
 class Hairsalon::Scraper
     
-    def styles
+    def scrape_services
         html = "http://cieloshairdesign.com/our_services"
         doc = Nokogiri::HTML(open(html))   
-        
+        binding.pry
        
     end
     # binding.pry
